ef983e2fda3ec92b9ae06f0c02a2eb09644f89ab
Mohammed Rady <74203569+mohamedwr@users.noreply.github.com>
2022-06-30 01:26:58 +0200

    closed tickets form

--- pages/tickets.vue
+++ pages/tickets.vue
@@ -2,11 +2,11 @@
   <div class="contents">
     <!--FragmentTimer title="CHECK OUR OFFERS!" class="mb-8" /-->
     <FragmentTicketsCountdown class="mb-8" />
-    <FragmentTicketsPackage class="mb-4 lg:mb-7" />
+    <!-- <FragmentTicketsPackage class="mb-4 lg:mb-7" />
     <FragmentTicketsInstructions class="mb-4 lg:mb-7" />
     <div class="box block w-full p-5 drop-shadow-md">
       <FragmentTicketsForm />
-    </div>
+    </div> -->
   </div>
 </template>
 
